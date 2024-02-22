bcd4bbf3867a3f2b7301ea423f372aa0b63873a6
Darvidor Doe <darvidor@proton.me>
2024-02-22 10:55:12 +0100

    PATCH 2024-02-22 - Minor problems in Config Class.
    The set method had an optional namespace parameter not well-defined. Fix the problem.

--- kernel/config/Iconfig.php
+++ kernel/config/Iconfig.php
@@ -7,6 +7,6 @@
 
         public function setNamespace(string $namespace);
         public function get(string $key, ?string $namespace);
-        public function set(string $key, mixed $value, ?string $namespace);
+        public function set(string $key, mixed $value, ?string $namespace = null);
     }
 ?>
